7a051c32ecf947bf38b40e0f56236bd1a855ffa0
김정현 <rlawjd147@gmail.com>
2020-06-24 13:02:51 +0900

    - add header footer navbar

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 // require
-const express = require('express')
-const app = express()
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-  res.send('express.js로 만든 server입니다.')
-})
+const app = express();
 
-app.listen(3000, () => {
-  console.log(`3000번 port에 http server를 띄웠습니다.`)
-})
+const mainUI = require("./controller/routes/mainui");
+
+const PORT = 3000;
+const URI = "localhost";
+
+app.set("views", path.join(__dirname, "views")); // views경로 설정
+app.set("view engine", "ejs"); // view엔진 지정
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  session({
+    key: "sid",
+    secret: "secret key", // 세션id 암호화할때 사용
+    resave: false, // 접속할때마다 id부여금지
+    saveUninitialized: true
+  })
+); // 세션id사용전에는 발급금지
+
+app.use("/", mainUI);
+
+app.listen(PORT, () => {
+  console.log("============================");
+  console.log(" 신호등제어시스템 웹 서버 ");
+  console.log(" 웹브라우저접속주소 : " + URI + ":" + PORT);
+  console.log("============================");
+});
